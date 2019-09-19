15419424cb94de771cd32b860faba42b77e5b16c
huanghaoxuan <huanghaoxuan1998@outlook.com>
2019-09-19 15:44:09 +0800

    创建bean容器

---
+++ src/main/java/com/hhx/core/BeanContainer.java
@@ -0,0 +1,95 @@
+package com.hhx.core;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: HuangHaoXuan
+ * @Email: huanghaoxuan1998@outlook.com
+ * @github https://github.com/huanghaoxuan
+ * @Date: 2019/9/19 14:25
+ * @Version 1.0
+ */
+
+/**
+ * Bean容器
+ */
+@Slf4j
+public class BeanContainer {
+    /**
+     * 存放所有Bean的Map
+     */
+    private final Map<Class<?>, Object> beanMap = new ConcurrentHashMap<>();
+
+    /**
+     * 获取Bean实例
+     */
+    public Object getBean(Class<?> clz) {
+        if (null == clz) {
+            return null;
+        }
+        return beanMap.get(clz);
+    }
+
+    /**
+     * 获取所有Bean集合
+     */
+    public Set<Object> getBeans() {
+        return new HashSet<>(beanMap.values());
+    }
+
+    /**
+     * 添加一个Bean实例
+     */
+    public Object addBean(Class<?> clz, Object bean) {
+        return beanMap.put(clz, bean);
+    }
+
+    /**
+     * 移除一个Bean实例
+     */
+    public void removeBean(Class<?> clz) {
+        beanMap.remove(clz);
+    }
+
+    /**
+     * Bean实例数量
+     */
+    public int size() {
+        return beanMap.size();
+    }
+
+    /**
+     * 所有Bean的Class集合
+     */
+    public Set<Class<?>> getClasses() {
+        return beanMap.keySet();
+    }
+
+    /**
+     * 通过注解获取Bean的Class集合
+     */
+    public Set<Class<?>> getClassesByAnnotation(Class<? extends Annotation> annotation) {
+        return beanMap.keySet()
+                .stream()
+                .filter(clz -> clz.isAnnotationPresent(annotation))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * 通过实现类或者父类获取Bean的Class集合
+     */
+    public Set<Class<?>> getClassesBySuper(Class<?> superClass) {
+        return beanMap.keySet()
+                .stream()
+                .filter(superClass::isAssignableFrom)
+                .filter(clz -> !clz.equals(superClass))
+                .collect(Collectors.toSet());
+    }
+}
